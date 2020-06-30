7259f84a5deaf74176ee2014b1b0344e53ebbaa1
Sangwoo Ra <robbyra@gmail.com>
2020-07-01 00:41:16 +0900

    Update acmicpc_17299.cpp

--- acmicpc/acmicpc_17299.cpp
+++ acmicpc/acmicpc_17299.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 using namespace std;
 
+// 각 숫자의 등장횟수를 저장하는 배열
 int freq[1000001];
 
 int main() {
@@ -19,4 +20,53 @@ int main() {
 		cin >> a[i];
 		freq[a[i]] += 1;
 	}
+
+	//예제 입력 : 1 1 2 3 4 2 1
+
+	// a[0] = 1, a[1] = 1, a[2] = 2, a[3] = 3, a[4] = 4, a[5] = 2, a[6] = 1
+	// freq[1] = 3, freq[2] = 2, freq[3] = 1, freq[4] = 1
+
+	stack<int> s;
+	s.push(0);
+	for (int i = 1; i < N; i++) {
+		if (s.empty()) {
+			s.push(i);
+		}
+		while (!s.empty() && freq[a[s.top()]] < freq[a[i]]) {
+			answer[s.top()] = a[i];
+			s.pop();
+		}
+		s.push(i);
+	}
+
+	// i = 1일 때 freq[1] = 3 < freq[1] = 3 -->false --> s : <0,1>
+	// i = 2일 때 freq[1] = 3 < freq[2] = 2 -->false --> s : <0,1,2>
+	// i = 3일 때 freq[2] = 2 < freq[3] = 1 -->false --> s : <0,1,2,3>
+	// i = 4일 때 freq[3] = 1 < freq[4] = 1 -->false --> s : <0,1,2,3,4>
+	// i = 5일 때 freq[4] = 1 < freq[2] = 2 -->true
+	  // answer[4] = 2, s : <0,1,2,3>
+	  // freq[3] = 1 < freq[2] = 2 --> true
+	  // answer[3] = 2, s : <0,1,2>
+	  // freq[2] = 2 < freq[2] = 2 --> false
+	  // s : <0,1,2,5>
+	// i = 6일 때 freq[2] = 2 < freq[1] = 3 --> true
+	  // answer[5] = 1, s : <0,1,2>
+	  // freq[2] = 2 < freq[1] = 3 --> true
+	  // answer[2] = 1, s : <0,1>
+	  // freq[1] = 3 < freq[1] = 3 --> false
+	  // s : <0,1,6>
+
+	while (!s.empty()) {
+		answer[s.top()] = -1;
+		s.pop();
+	}
+	// answer[6] = -1
+	// answer[1] = -1
+	// answer[0] = -1
+
+	for (int i = 0; i < N; i++) {
+		cout << answer[i] << ' ';
+	}
+	cout << '\n';
+	return 0;
 }
