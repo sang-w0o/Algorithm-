9521cf6298c0b8cf3b6093c0360181bea4028ee8
Sangwoo Ra <robbyra@gmail.com>
2020-07-12 01:18:04 +0900

    Update acmicpc_11403.cpp

--- acmicpc/acmicpc_11403.cpp
+++ acmicpc/acmicpc_11403.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
-#include <cstring>
-
+#include <queue>
 using namespace std;
 
-bool visited[101];
-
-void dfs(int **adj, int vertex, int size, queue<int> q) {
+void bfs(int **adj, int vertex, int size) {
 	
+	int visited[101] = {0, };
+	
+	queue<int> q;
+	
+	q.push(vertex);
+	
+	while(!q.empty()) {
+		int tmp = q.front();
+		q.pop();
+		for(int i = 1; i <= size; i++) {
+			// 아래 if문은 tmp와 i 사이의 간선이 있고,
+			// i번째를 방문한적이 없다면 통과된다. 
+			if(adj[tmp][i] && !visited[i]) {
+				q.push(i);
+				visited[i] = true;
+				adj[vertex][i] = 1;
+			}	
+		}
+	}
 } 
+
 int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
 	
 	int N;
 	cin >> N;
@@ -24,13 +44,23 @@ int main() {
 		}
 	}
 	
-	queue<int> q;
-	
 	for(int i = 1; i <= N; i++) {
-		bfs(adj, i, N, q);
-		memset(visited, false, sizeof(visited));
-		
+		bfs(adj, i, N);
 	}
 	
+	for(int i = 1; i <= N; i++) {
+		for(int j = 1; j <= N; j++) {
+			cout << adj[i][j] << " ";
+		}
+		cout << '\n';
+	}
+	
+	for(int i = 0; i <= N; i++) {
+		delete[] adj[i];
+	}
+	
+	delete[] adj;
+	
+	return 0;
 	
 }
