3b7865f8e3d5c1c86d90bc545e4835042af7839e
Sangwoo Ra <robbyra@gmail.com>
2020-07-14 16:03:41 +0900

    Update acmicpc_2263.cpp

--- acmicpc/acmicpc_2263.cpp
+++ acmicpc/acmicpc_2263.cpp
@@ -1,87 +1,68 @@
 #include <iostream>
 using namespace std;
 
-typedef struct Node {
-	int data;
-	struct Node *left;
-	struct Node *right;
-}Node;
+#define MAX 100001
+// position : 입력으로 들어온 중위순회의 결과를 저장한 inOrder배열에서
+// 각 원소가 몇 번째 index에 저장되어 있는지를 기록한다. 
+int position[MAX];
+// inOrder : 입력으로 주어지는 중위순회의 결과를 저장하는 배열 
+int inOrder[MAX];
+// postOrder : 입력으로 주어지는 후위순회의 결과를 저장하는 배열 
+int postOrder[MAX];
 
-void preOrder(Node *parent) {
-	if(parent != NULL) {
-		cout << parent->data << " ";
-		preOrder(parent->left);
-		preOrder(parent->right);
+// inOrderStart : inOrder값이 들어간 배열의 시작 index ( = 0)
+// inOrderEnd : inOrder값이 들어간 배열의 마지막 index ( = n-1)
+// postOrderStart : postOrder값이 들어간 배열의 시작 index ( = 0)
+// postOrderEnd : postOrder값이 들어간 배열의 마지막 index ( = n-1)
+// 위 값들은 root기준으로 왼쪽 subtree, 오른쪽 subtree를 구분해서 재귀호출하기 위해 사용된다. 
+void solve(int inOrderStart, int inOrderEnd, int postOrderStart, int postOrderEnd) {
+	if(inOrderStart > inOrderEnd || postOrderStart > postOrderEnd) {
+		return;
 	}
-}
-
-
-int main() {
 	
-	ios_base::sync_with_stdio(false);
+	// root는 항상 postOrder의 결과의 마지막이다. 
+	int root = postOrder[postOrderEnd];
+	cout << root << " ";
+	
+	// p는 inOrder배열에서 root가 위치한 index를 저장한다. 
+	int p = position[root];
+	
+	// left : root 기준 왼쪽 subtree를 가리킨다.
+	// 왼쪽 subtree는 inOrder 배열에서 root보다 왼쪽에 있는 노드로 구성된 트리 이다. 
+	int left = p - inOrderStart;
+	
+	// p - 1 : root보다 왼쪽까지 순회해야 하기 때문
+	// postOrder+left-1 : 이 값은 root의 왼쪽 subtree를 구성하는 원소들의 index값의 마지막 범위이다. 
+	solve(inOrderStart, p-1, postOrderStart, postOrderStart+left-1);
+	
+	// p + 1 : root기준 오른쪽 subtree를 조사하기 위함.
+	// postOrderStart + left : 마찬가지로 root기준 오른쪽 subtree를 조사하기 위함. 
+	// postOrderEnd - 1 : 배열이 0-base이기 때문에 -1을 해준다. 
+	solve(p+1, inOrderEnd, postOrderStart + left, postOrderEnd-1);
+}
+int main() {
+	ios::sync_with_stdio(false);
 	cin.tie(0), cout.tie(0);
 	
 	int n;
 	cin >> n;
 	
-	int *inOrder = new int[n+1];
-	int *postOrder = new int[n+1];
-	
-	for(int i = 1; i <= n; i++) {
+	for(int i = 0; i < n; i++) {
 		cin >> inOrder[i];
 	}
 	
-	for(int i = n; i >= 1; i--) {
+	for(int i = 0; i < n; i++) {
 		cin >> postOrder[i];
 	}
 	
-//	Node *root = new Node;
-//	root->data = postOrder[1];
-//	root->left = NULL;
-//	root->right = NULL;
-	Node *root = NULL;
-	
-	for(int i = 1; i <= n; i++) {
-		Node *node = new Node;
-		node->data = postOrder[i];
-		node->left = NULL;
-		node->right = NULL;
-		
-		if(root == NULL) {
-			root = node;
-		}
-		else {
-			Node *curNode = root;
-			Node *prev;
-			while(true) {
-				if(postOrder[i] > curNode->data) {
-					prev = curNode;
-					if(curNode->right != NULL) {
-						curNode = curNode->right;
-					}
-					else {
-						curNode->right = node;
-						break;
-					}
-				}
-				else {
-					prev = curNode;
-					if(curNode->left != NULL) {
-						curNode = curNode->left;
-					}
-					else {
-						curNode->left = node;
-						break;
-					}
-				}
-			}
-		}
+	for(int i = 0; i < n; i++) {
+		position[inOrder[i]] = i;
 	}
 	
-	//cout << root->data << " ";
-	preOrder(root);
-	cout << '\n';
-	delete[] inOrder;
-	delete[] postOrder;
-	return 0;
+	// 0 : inOrder배열의 시작 index
+	// n-1 : inOrder배열의 끝 index
+	// 0 : postOrder배열의 시작 index
+	// n-1 : inOrder배열의 끝 index 
+	solve(0,n-1,0,n-1);
+	
 }
