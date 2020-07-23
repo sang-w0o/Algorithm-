13f65319211e654486119a1a8f9ccfcd72fac664
Sangwoo Ra <robbyra@gmail.com>
2020-07-23 15:55:40 +0900

    Create acmicpc_11722.cpp

---
+++ acmicpc/acmicpc_11722.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#define MAX 1001
+
+int numbers[MAX];
+int D[MAX];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+        cin >> numbers[i];
+    }
+
+    int res = 0;
+
+    for(int i = 1; i <= N; i++) {
+        D[i] = numbers[i];
+        for(int j = 1; j <= i; j++) {
+            if(numbers[j] < numbers[i] && D[i] < D[j] + numbers[i]) {
+                D[i] = D[j] + numbers[i];
+            }
+           if(res < D[i]) res = D[i];
+        }
+    }
+
+    cout << res << '\n';
+    return 0;
+}
+
+
+
