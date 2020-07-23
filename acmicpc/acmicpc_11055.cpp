0b9ef20c08136ae029dd8ac000dbd877df82216f
Sangwoo Ra <robbyra@gmail.com>
2020-07-23 15:55:14 +0900

    Update acmicpc_11055.cpp

--- acmicpc/acmicpc_11055.cpp
+++ acmicpc/acmicpc_11055.cpp
@@ -20,7 +20,7 @@ int main() {
 
     for(int i = 1; i <= N; i++) {
         D[i] = numbers[i];
-        for(int j = 1; j < i; j++) {
+        for(int j = 1; j <= i; j++) {
             if(numbers[j] < numbers[i] && D[i] < D[j] + numbers[i]) {
                 D[i] = D[j] + numbers[i];
             }
